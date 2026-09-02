e5bd4fdbfd8a753dd196866e1b3cc8ca9ca13e16
0Zyyra0 <am49733794@gmail.com>
2026-09-02 21:29:21 +0330

    تمرین مباحث پایگاه‌داده و ادمین - بخش سوم
    در مثال snippet و truncat اشاره شد که می‌توانید بر اساس تعداد کارکتر ها از نمایش بیش از حد اطلاعات متون جلوگیری نمایید. اما چگونه می‌توان به جای کارکتر بر اساس تعداد کلمات این فیلتر را اعمال کرد. تابع مربوطه را جایگزین عملکرد فعلی نمایید.
    راهنمایی: برای درک بهتر این عملکرد بهتر است از کلید واژه  function excerpt در جست و جو های خود استفاده نمایید.
    
    ** حتما حتما در زمان ارسال پاسخ تمرین، فایل DataBase خود را نیز ارسال کنید.

--- website/models.py
+++ website/models.py
@@ -1,4 +1,17 @@
 from django.db import models
+from django.db import models
+from django.utils.text import Truncator
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField()
+    counted_view = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+
 
 
 class Post(models.Model):
@@ -9,3 +22,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def excerpt(self, word_count=20):
+        return Truncator(self.content).words(word_count, truncate='...')
